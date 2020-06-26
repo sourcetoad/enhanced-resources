a671541b75763b03da85705bde3076c3d373b03c
Jason Jones <jason.erik.jones+github@gmail.com>
2020-06-25 20:59:25 -0400

    feat: add toResponse method to Resourceable to allow for easier conversion to a response

--- src/Support/Concerns/Resourceable.php
+++ src/Support/Concerns/Resourceable.php
@@ -4,14 +4,35 @@ declare(strict_types=1);
 
 namespace Sourcetoad\EnhancedResources\Support\Concerns;
 
+use Illuminate\Http\JsonResponse;
 use Sourcetoad\EnhancedResources\EnhancedResource;
 
 trait Resourceable
 {
-    public function toResource(?string $resourceClass): EnhancedResource
+    public function toResource(?string $resourceClass = null): EnhancedResource
     {
         $resourceClass ??= static::$resourceClass ?? EnhancedResource::class;
 
         return $resourceClass::make($this);
+    }
+
+    public function toResponse(
+        ?string $resourceClass = null,
+        ?string $format = null,
+        ?int $statusCode = null
+    ): JsonResponse {
+        $resource = $this->toResource($resourceClass);
+
+        if ($format !== null) {
+            $resource = $resource->format($format);
+        }
+
+        $response = $resource->response();
+
+        if ($statusCode !== null) {
+            $response = $response->setStatusCode($statusCode);
+        }
+
+        return $response;
     }
 }
--- src/Support/Contracts/Resourceable.php
+++ src/Support/Contracts/Resourceable.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Sourcetoad\EnhancedResources\Support\Contracts;
 
+use Illuminate\Http\JsonResponse;
 use Sourcetoad\EnhancedResources\EnhancedResource;
 
 interface Resourceable
 {
     public function toResource(?string $resourceClass = null): EnhancedResource;
+    public function toResponse(
+        ?string $resourceClass = null,
+        ?string $format = null,
+        ?int $statusCode = null
+    ): JsonResponse;
 }
